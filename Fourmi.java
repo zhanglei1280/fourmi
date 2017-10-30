97dd729854ae2d8f63858403d05f816211089a70
zhanglei <1280368848@qq.com>
2017-10-30 19:18:43 +0100

    fist time

---
+++ Fourmi.java
@@ -0,0 +1,24 @@
+
+
+public class Fourmi
+{
+
+	public static void choisirVilleSuivante(int ville)
+	int nbville = 20;
+	
+	
+	for(int i = 0; i < 20; i++) // pour chaque fourmi faire
+	{
+		ArrayList<Int> villelist = new ArrayList<Int>();
+		villepasse = new ArrayList<Int>();
+		ArrayList<Int> = new villepasse.add(i);
+		villelist.remove(i+1);
+		while(villepasse.size() <= 20)
+		{
+			choisirVilleSuivante(i+1);
+			villepasse.add(i+1);
+			
+		}
+
+	}
+}
